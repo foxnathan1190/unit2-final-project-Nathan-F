1a563325fcad88275e548e17217830c2e6ba9e1c
foxnathan1190 <foxnathan1190@gmail.com>
2025-10-28 21:15:45 -0500

    Made profile page editable for user.

--- beers-and-breweries/src/components/ProfilePage.jsx
+++ beers-and-breweries/src/components/ProfilePage.jsx
@@ -7,13 +7,38 @@ import "./ProfilePage.css";
 const ProfilePage = ({ isLoggedIn }) => {
 
     const [profile, setProfile] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
 
-    useEffect(() => {    // This pulls the data from local storage for the profil to be built below.
+    useEffect(() => {    // This pulls the data from local storage for the profile to be built below.
         const storedProfile = localStorage.getItem('userProfile');
         if (storedProfile) {
             setProfile(JSON.parse(storedProfile));
         }
     }, []);
+
+    const [editedProfileData, setEditedProfileData] = useState({ ...profile });
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+        setEditedProfileData({ ...profile }); // Initialize edited data with current profile data
+    };
+
+    const handleSaveClick = async () => {
+        setProfile(editedProfileData); // Update the main profile data
+        setIsEditing(false);
+    };
+
+    const handleCancelClick = () => {
+        setIsEditing(false);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEditedProfileData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
 
     if (isLoggedIn) {
         return (
@@ -60,37 +85,59 @@ const ProfilePage = ({ isLoggedIn }) => {
                 <Header />
                 <NavigationMenu />
                 <div className="layout">
-                    {profile ? (              // This ternary creates the profile if theres data.
+                    {profile ? (
                         <>
-                            <h1>Your Profile</h1>
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <td>Name:</td>
-                                        <td>{profile.fName} {profile.lName}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Username:</td>
-                                        <td>{profile.username}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Email:</td>
-                                        <td>{profile.email}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Password:</td>
-                                        <td>{profile.password}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Favorite Brewery:</td>
-                                        <td>{profile.favBrewery}</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Birthday:</td>
-                                        <td>{profile.month}/{profile.day}/{profile.year}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            {isEditing ? (              // This ternary allows the user to edit their profile data.
+                                <>
+                                    <label htmlFor="fName">First Name:</label><br />
+                                    <input type="text" id="fName" name="fName" value={editedProfileData.fName} onChange={handleChange} required></input><br />
+                                    <label htmlFor="lName">Last Name:</label><br />
+                                    <input type="text" id="lName" name="lName" value={editedProfileData.lName} onChange={handleChange} required></input><br />
+                                    <label htmlFor="username">Username:</label><br />
+                                    <input type="text" id="username" name="username" value={editedProfileData.username} onChange={handleChange} required></input><br />
+                                    <label htmlFor="email">Email:</label><br />
+                                    <input type="email" id="email" name="email" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" value={editedProfileData.email} onChange={handleChange} required></input><br />
+                                    <label htmlFor="password">Password: </label><br />
+                                    <input type="password" id="password" name="password" value={editedProfileData.password} onChange={handleChange} required></input><br />
+                                    <label htmlFor="faveBrewery">Favorite Brewery:</label><br />
+                                    <input type="text" id="favBrewery" name="favBrewery" value={editedProfileData.favBrewery} onChange={handleChange}></input><br />
+                                    <button onClick={handleSaveClick}>Save</button>
+                                    <button onClick={handleCancelClick}>Cancel</button>
+                                </>
+                            ) : (
+                                <>
+                                    <h1>Your Profile</h1>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td>Name:</td>
+                                                <td>{profile.fName} {profile.lName}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Username:</td>
+                                                <td>{profile.username}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Email:</td>
+                                                <td>{profile.email}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Password:</td>
+                                                <td>{profile.password}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Favorite Brewery:</td>
+                                                <td>{profile.favBrewery}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Birthday:</td>
+                                                <td>{profile.month}/{profile.day}/{profile.year}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <button onClick={handleEditClick}>Edit Profile</button>
+                                </>
+                            )}
                         </>
                     ) : (
                         <p>No Profile Found</p>
